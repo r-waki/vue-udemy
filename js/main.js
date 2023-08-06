f5dba3ddadd79b20c98634668a6e04ae8a948bb3
r-waki <r.wakeyii@gmail.com>
2023-08-06 19:10:55 +0900

    feat(todo app): complete

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
 const app=Vue.createApp({
      data:()=>({
-         message: "Hello, Vue.js!"
-     })
+        newItem: '',
+        todos: []
+     }),
+     methods: {
+        addItem: function(event){
+            //console.log("clicked!")
+            if (this.newItem === "") return
+            let todo = {
+                item: this.newItem,
+                isDone: false
+            }
+            this.todos.push(todo)
+            this.newItem=""
+        },
+        deleteItem: function(index){
+            console.log("delete")
+            console.log(index)
+            this.todos.splice(index, 1)
+        }
+     }
+
 })
 
 app.component("hello-component", {
