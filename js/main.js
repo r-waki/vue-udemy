c9773a2ae341b4289cb1ae6d99fca9a2d1871f87
r-waki <r.wakeyii@gmail.com>
2023-08-19 17:50:48 +0900

    feat(template): 空の状態

--- js/main.js
+++ js/main.js
@@ -1,30 +1,6 @@
 const app=Vue.createApp({
      data:()=>({
-        newItem: '',
-        todos: []
      }),
-     methods: {
-        addItem: function(event){
-            //console.log("clicked!")
-            if (this.newItem === "") return
-            let todo = {
-                item: this.newItem,
-                isDone: false
-            }
-            this.todos.push(todo)
-            this.newItem=""
-        },
-        deleteItem: function(index){
-            console.log("delete")
-            console.log(index)
-            this.todos.splice(index, 1)
-        }
-     }
-
-})
-
-app.component("hello-component", {
-     template: '<p>Hello</p>'
 })
 
 app.mount('#app')
