3271db59e15f45a31090307513234f574788a06e
r-waki <r.wakeyii@gmail.com>
2023-08-19 19:59:13 +0900

    feat(api): qiitaからデータ取得実施まで

--- js/main.js
+++ js/main.js
@@ -1,7 +1,45 @@
 const app=Vue.createApp({
      data:()=>({
+          items: null,
+          keyword: '',
+          message: ''
      }),
+     watch: {
+
+     },
+     mounted: function(){
+          this.keyword = 'JavaScript'
+          this.getAnswer()
+     },
+     methods: {
+          getAnswer: function(){
+               if(this.keyword === ''){
+                    console.log('karamoji')
+                    this.items = null
+                    return
+               }
+
+               this.message = 'Loading...'
+               const vm = this
+               const params = { page:1, per_page:20, query: this.keyword }
+
+               axios.get('https://qiita.com/api/v2/items', { params })
+                    .then(function(response){
+                         //console.log(response)
+                         vm.items = response.data
+                    })
+                    .catch(function(error){
+                         vm.message = 'Error!' + error
+                    })
+                    .finally(function(){
+                         vm.message = ''
+                    })
+          }
+
+     },
+     computed: {
+
+     }
 })
 
 app.mount('#app')
- 
